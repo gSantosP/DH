39bb05b8437d36f8479cc7f40adf78e292092c4c
GabrielSantosPereira <gabriel7132@protonmail.com>
2021-06-21 18:20:07 -0300

    Aula08

---
+++ prog_imp/Aula_8_Objetos/exs1.js
@@ -0,0 +1,14 @@
+// microdeafios
+// "Meu cardapio"
+var cardapio = ["Feijoada", "Risoto", "Lagosta Grelhada",
+"Salmão", "Strogonoff"]
+function entrada(){
+    console.log("Seja bem vindo!\nRestaurante: 'Só coisa boa'")
+    console.log(cardapio)
+}
+
+
+function Restaurante (nome, cardapio){
+    entrada()
+}
+Restaurante()
---
+++ prog_imp/Aula_8_Objetos/exs2.js
@@ -0,0 +1,9 @@
+// microdesafios
+// Exercicio "Ver numeros"
+
+var numeroPrimos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+numeros [n1, n2, n3, n4, n5, n6, n7, n8, n9, n10] = numerosPrimos
+function maiorNumero (n){
+  return Math.min(numeroPrimos)
+}
+
