32a93c3b747b913facd47d828c68188c1d6cefe3
GabrielSantosPereira <gabriel7132@protonmail.com>
2021-07-08 11:20:20 -0300

    correcao exs3

--- prog_imp/Aula_10_arraysAvancados/exs3.js
+++ prog_imp/Aula_10_arraysAvancados/exs3.js
@@ -3,5 +3,7 @@
 // .reduce() para devolver uma string com os números formatados.
 // Exemplo [1,5,9,3,7] => “1 – 5 – 9 – 3 – 7”
 var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-let str = numeros.reduce(" - ")
-console.log(numeros)
+var str = numeros.reduce(function(acumulador, numero){
+    return ""+acumulador+"-"+numero
+})
+console.log(str)
